37f5ee24fa0ea494700eb967f4552f280ef62cb6
Iyad <iyad.mahdy@hotmail.com>
2025-09-25 19:29:27 +0300

    Adjusting for hosting

--- services/llm.py
+++ services/llm.py
@@ -2,13 +2,14 @@ import os
 from dotenv import dotenv_values
 from langchain_groq import ChatGroq
 
-env = dotenv_values("app.env")
-os.environ["GROQ_API_KEY"] = env["GROQ_API_KEY"]
-os.environ["HF_TOKEN"] = env["HF_TOKEN"]
+# env = dotenv_values("app.env")
+GROQ_API_KEY  = os.getenv("GROQ_API_KEY")
+HF_TOKEN  = os.getenv("HF_TOKEN")
 
 def load_llm():
     return ChatGroq(
         model="qwen/qwen3-32b",
+        api_key=GROQ_API_KEY,
         temperature=0.3,
         max_tokens=256,
         reasoning_effort="none"
