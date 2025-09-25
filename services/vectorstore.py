571842d5b185c44cb605d793322ba98fe02e9e5d
Iyad <iyad.mahdy@hotmail.com>
2025-09-25 20:14:03 +0300

    Optimizing for memory

--- services/vectorstore.py
+++ services/vectorstore.py
@@ -2,7 +2,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 
 def load_vectorstore():
-    embeddings = HuggingFaceEmbeddings(model_name="Qwen/Qwen3-Embedding-0.6B")
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     return FAISS.load_local(
         "data/parenting_faiss_qwen3",
         embeddings,
